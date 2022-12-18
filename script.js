9384b80ddf9eefb4eeab66b0f81a72b57c20ad2a
Christ-M <minassianchrist@gmail.com>
2022-12-18 09:20:09 +0330

    Added a new line code for toast

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const bootstrap = require("bootstrap");
+// const bootstrap = require("bootstrap");
 
-let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-let tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl){
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-});
+// let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+// let tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl){
+//     return new bootstrap.Tooltip(tooltipTriggerEl)
+// });
 
-let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+// let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+// let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+//   return new bootstrap.Popover(popoverTriggerEl)
+// })
+
+document.getElementById("toastbtn").onclick = function () {
+  let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+  let toastList = toastElList.map(function(toastEl){
+    return new bootstrap.Toast(toastEl)
+  })
+  toastList.forEach(toast => toast.show())
+};
